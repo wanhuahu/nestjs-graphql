583e12ce287548b7feaaea4b6f1cd2f970d4eace
Wanhua Hu <wanhuahu@gmail.com>
2025-08-13 22:51:29 -0400

    mutation

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,6 +1,7 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './user.model';
 import { UserService } from './user.service';
+import { CreateUserInput } from './dto/create-user.input';
 
 @Resolver(() => User)
 export class UserResolver {
@@ -9,5 +10,10 @@ export class UserResolver {
   @Query(() => User, { nullable: true })
   user(@Args('id') id: string): User | undefined {
     return this.userService.getUserById(id);
+  }
+
+  @Mutation(() => User)
+  createUser(@Args('input') input: CreateUserInput): User {
+    return this.userService.createUser(input);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './user.model';
+import { CreateUserInput } from './dto/create-user.input';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class UserService {
@@ -10,5 +12,14 @@ export class UserService {
 
   getUserById(id: string): User | undefined {
     return this.users.find(user => user.id === id);
+  }
+
+  createUser(input: CreateUserInput): User {
+    const newUser: User = {
+      id: randomUUID(),
+      ...input,
+    };
+    this.users.push(newUser);
+    return newUser;
   }
 }
